7dd8d72ca32546b64b1593f09362c8be7e8994fd
sakil-sagor <sakilsagor76543@gmail.com>
2024-10-21 12:02:51 +0600

    update

--- src/components/shared/SectionTitle/SectionTitle.jsx
+++ src/components/shared/SectionTitle/SectionTitle.jsx
@@ -1,8 +1,13 @@
 const SectionTitle = ({ title, subTitle }) => {
   return (
-    <div className="text-center font-bold pt-12 pb-24">
-      <h2 className="uppercase text-center text-[#003366] text-xl">{title}</h2>
-      <h3 className="text-4xl mt-4">{subTitle}</h3>
+    <div className="text-center font-bold pt-12 pb-24 relative overflow-hidden">
+      <div class="absolute top-[50%] left-[50%] md:left-[20%] lg:left-[20%] xl:left-[10%] w-[450px] h-[450px] bg-gradient-to-tr from-red-900 to-transparent rounded-full blur-md"></div>
+      <div>
+        <h2 className="uppercase text-center text-[#003366] text-xl">
+          {title}
+        </h2>
+        <h3 className="text-4xl mt-4">{subTitle}</h3>
+      </div>
     </div>
   );
 };
