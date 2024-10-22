e6d08034dad631b94a5be13c645b870dff7913ef
husneara <husnearaa99@gmail.com>
2024-10-22 10:51:40 +0600

    has cteaded the our team component has added the eader part of our team section

--- src/app/(withCommonLayout)/aboutUs/page.js
+++ src/app/(withCommonLayout)/aboutUs/page.js
@@ -1,4 +1,5 @@
 import OurPromises from '@/components/FrontendComponent/AboutUs/OurPromises';
+import OurTeam from '@/components/FrontendComponent/AboutUs/OurTeam';
 import WhyChooseUs from '@/components/FrontendComponent/AboutUs/WhyChooseUs';
 import WorkingProcess from '@/components/FrontendComponent/AboutUs/WorkingProcess';
 import React from 'react';
@@ -8,6 +9,7 @@ const AboutUs = () => {
         <div className='mt-36'>
             <WhyChooseUs />
             <WorkingProcess />
+            <OurTeam />
             <OurPromises />
         </div>
     );
---
+++ src/components/FrontendComponent/AboutUs/OurTeam.jsx
@@ -0,0 +1,15 @@
+
+const OurTeam = () => {
+    return (
+        <div className="mt-20">
+            <section>
+                <div className="text-center">
+                    <h1 className="lg:text-lg text-sm text-[#003366] font-bold">OUR PROFESSIONALS</h1>
+                    <h2 className="lg:text-3xl text-lg text-black font-bold pt-4">Our Dedicated Team</h2>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default OurTeam;
